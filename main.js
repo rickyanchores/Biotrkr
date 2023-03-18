027ef6d5b39919b9774cb1b4d23aec5627dd7fb8
RickyAnchores <ricky23anchores@gmail.com>
2023-03-18 18:56:51 +0000

    final v1

---
+++ main.js
@@ -0,0 +1,69 @@
+/*
+// VARIABLES
+let weight = document.querySelector(".weight").value
+let height = document.querySelector(".height").value
+let age = document.querySelector(".age").value
+*/
+
+//BUTTON
+let btn = document.querySelector(".calculate")
+// RESULTS BMR
+let result = document.querySelector(".resultBMR")
+let bmrNormal = document.querySelector(".bmr-normal")
+let bmrLow = document.querySelector(".bmr-low")
+// RESULT BMI
+let bmiResult = document.querySelector(".bmi-result")
+
+
+/*
+
+// create person class
+class Person{
+	constructor(weight,height,age){
+  	  this.weight =	weight;
+			this.height = height;
+			this.age = age;
+  }
+}
+
+const Ricky = new Person(90,173,30)
+
+*/
+
+// FUNCTION CALCULATE BMR
+const calculateBMR = (weight,height,age) => {
+
+  this.weight =	weight;
+	this.height = height;
+	this.age = age;
+
+  let bmr = weight * 10 + height * 6.25 - age * 4.92 - 5
+  console.log(bmr)
+  
+  bmrNormal.innerHTML = bmr
+  bmrLow.innerHTML = bmr * 1.2
+}
+// FUNCTION CALCULATE BMI 
+const calculateBMI = (weight,height) => {
+	let bmi = (weight / height / height) * 10000
+  console.log(bmi)
+  bmiResult.innerHTML = bmi
+}
+
+
+
+//BTN fire function
+btn.addEventListener("click", () => {
+	console.log("FIRE")
+  
+  // VARIABLES
+  let weight = document.querySelector(".weight").value
+  let height = document.querySelector(".height").value
+  let age = document.querySelector(".age").value
+  
+  calculateBMR(parseInt(weight),parseInt(height),parseInt(age))
+  calculateBMI(parseInt(weight),parseInt(height))
+})
+
+
+
